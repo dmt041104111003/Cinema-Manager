b5ff0ea8309e9b612afe57bf9ee60847fd9cdbdc
dmt041104111003 <daomanhtung4102003@gmail.com @example.com>
2025-03-22 05:44:41 +0700

    cấu hình build.gradle của app và kết nối firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,16 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.services)
 }
 
 android {
     namespace = "com.example.cinemamanager"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.cinemamanager"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -24,7 +25,12 @@ android {
                 "proguard-rules.pro"
             )
         }
+
     }
+    buildFeatures {
+        viewBinding = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -32,7 +38,9 @@ android {
 }
 
 dependencies {
-
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.database)
+    implementation(libs.firebase.auth)
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
@@ -40,4 +48,34 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    // Glide (Xử lý ảnh)
+    implementation(libs.glide)
+
+    // UI Components
+    implementation(libs.material.dialogs)
+    implementation(libs.circleimageview)
+    implementation(libs.circleindicator)
+
+    // Công cụ hỗ trợ
+    implementation(libs.gson)
+    implementation(libs.eventbus)
+    implementation(libs.exoplayer)
+    implementation(libs.flowlayout)
+    implementation(libs.paypal)
+
+    // QR Code Scanner
+    implementation(libs.zxing.core)
+    implementation(libs.zxing.embedded)
+
+    // Unit Tests
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
 }
+configurations {
+    create("cleanedAnnotations")
+    implementation {
+        exclude(group = "org.jetbrains", module = "annotations")
+    }
+}
+
