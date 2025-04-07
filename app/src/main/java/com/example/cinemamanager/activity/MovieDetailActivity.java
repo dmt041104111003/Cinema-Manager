6d81340f16d779313bfa50d44131f509af512512
MinhQuan <daomquan2004@gmail.com>
2025-04-08 00:25:32 +0700

    activity chitiet2

--- app/src/main/java/com/example/cinemamanager/activity/MovieDetailActivity.java
+++ app/src/main/java/com/example/cinemamanager/activity/MovieDetailActivity.java
@@ -71,5 +71,77 @@ public class MovieDetailActivity extends AppCompatActivity {
                 });
     }
 
+    private void displayDataMovie() {
+        if (mMovie == null) {
+            return;
+        }
+        GlideUtils.loadUrl(mMovie.getImage(), mActivityMovieDetailBinding.imgMovie);
+        mActivityMovieDetailBinding.tvTitleMovie.setText(mMovie.getName());
+        mActivityMovieDetailBinding.tvCategoryName.setText(mMovie.getCategoryName());
+        mActivityMovieDetailBinding.tvDateMovie.setText(mMovie.getDate());
+        String strPrice = mMovie.getPrice() + ConstantKey.UNIT_CURRENCY_MOVIE;
+        mActivityMovieDetailBinding.tvPriceMovie.setText(strPrice);
+        mActivityMovieDetailBinding.tvDescriptionMovie.setText(mMovie.getDescription());
 
+        if (!StringUtil.isEmpty(mMovie.getUrl())) {
+            Log.e("Movie Url", mMovie.getUrl());
+            initExoPlayer();
+        }
+    }
+
+    private void initListener() {
+        mActivityMovieDetailBinding.imgBack.setOnClickListener(view -> onBackPressed());
+        mActivityMovieDetailBinding.btnWatchTrailer.setOnClickListener(view -> scrollToLayoutTrailer());
+        mActivityMovieDetailBinding.imgPlayMovie.setOnClickListener(view -> startVideo());
+        mActivityMovieDetailBinding.btnBooking.setOnClickListener(view -> onClickGoToConfirmBooking());
+    }
+
+    private void onClickGoToConfirmBooking() {
+        if (mMovie == null) {
+            return;
+        }
+        if (DateTimeUtils.convertDateToTimeStamp(mMovie.getDate()) < DateTimeUtils.getLongCurrentTimeStamp()) {
+            Toast.makeText(this, getString(R.string.msg_movie_date_invalid), Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(ConstantKey.KEY_INTENT_MOVIE_OBJECT, mMovie);
+        GlobalFunction.startActivity(this, ConfirmBookingActivity.class, bundle);
+    }
+
+    private void scrollToLayoutTrailer() {
+        long duration = 500;
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+            float y = mActivityMovieDetailBinding.labelMovieTrailer.getY();
+            ScrollView sv = mActivityMovieDetailBinding.scrollView;
+            ObjectAnimator objectAnimator = ObjectAnimator.ofInt(sv, "scrollY", 0, (int) y);
+            objectAnimator.start();
+
+            startVideo();
+        }, duration);
+    }
+
+    private void initExoPlayer() {
+        PlayerView mExoPlayerView = mActivityMovieDetailBinding.exoplayer;
+
+        if (mPlayer != null) {
+            return;
+        }
+        mPlayer = new SimpleExoPlayer.Builder(this).build();
+        // Set player
+        mExoPlayerView.setPlayer(mPlayer);
+        mExoPlayerView.hideController();
+    }
+
+    private void startVideo() {
+        mActivityMovieDetailBinding.imgPlayMovie.setVisibility(View.GONE);
+        if (mPlayer != null) {
+            MediaItem mediaItem = MediaItem.fromUri(mMovie.getUrl());
+            mPlayer.addMediaItem(mediaItem);
+            // Prepare video source
+            mPlayer.prepare();
+            // Set play video
+            mPlayer.setPlayWhenReady(true);
+        }
+    }
 }
