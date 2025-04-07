c2e6fc260c582e6a498e2eb2b1a21c11d1a996ff
dmt041104111003 <daomanhtung4102003@gmail.com @example.com>
2025-04-07 23:27:16 +0700

    dn const

---
+++ app/src/main/java/com/example/cinemamanager/constant/ConstantKey.java
@@ -0,0 +1,8 @@
+package com.example.cinemamanager.constant;
+
+public class ConstantKey {
+
+    public static final String ADMIN_EMAIL_FORMAT = "@admin.com";
+    public static final String PREF_KEY_USER = "PREF_KEY_USER";
+    public static final String PREF_KEY_IS_LOGGED = "PREF_KEY_IS_LOGGED"; 
+}
