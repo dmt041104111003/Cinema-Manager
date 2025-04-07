bde5a400ee6877a9d0a2d596382315f6f0651325
MinhQuan <daomquan2004@gmail.com>
2025-04-08 00:31:27 +0700

    CRUD operations cho phim và lịch chiếu_2

--- app/src/main/java/com/example/cinemamanager/activity/admin/AddMovieActivity.java
+++ app/src/main/java/com/example/cinemamanager/activity/admin/AddMovieActivity.java
@@ -130,5 +130,99 @@ public class AddMovieActivity extends BaseActivity {
         }
     }
 
+    private void addOrEditMovie() {
+        String strName = mActivityAddMovieBinding.edtName.getText().toString().trim();
+        String strDescription = mActivityAddMovieBinding.edtDescription.getText().toString().trim();
+        String strPrice = mActivityAddMovieBinding.edtPrice.getText().toString().trim();
+        String strDate = mActivityAddMovieBinding.tvDate.getText().toString().trim();
+        String strImage = mActivityAddMovieBinding.edtImage.getText().toString().trim();
+        String strImageBanner = mActivityAddMovieBinding.edtImageBanner.getText().toString().trim();
+        String strVideo = mActivityAddMovieBinding.edtVideo.getText().toString().trim();
 
+        if (mCategorySelected == null || mCategorySelected.getId() <= 0) {
+            Toast.makeText(this, getString(R.string.msg_category_movie_require), Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (StringUtil.isEmpty(strName)) {
+            Toast.makeText(this, getString(R.string.msg_name_movie_require), Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (StringUtil.isEmpty(strDescription)) {
+            Toast.makeText(this, getString(R.string.msg_description_movie_require), Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (StringUtil.isEmpty(strPrice)) {
+            Toast.makeText(this, getString(R.string.msg_price_movie_require), Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (StringUtil.isEmpty(strDate)) {
+            Toast.makeText(this, getString(R.string.msg_date_movie_require), Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (StringUtil.isEmpty(strImage)) {
+            Toast.makeText(this, getString(R.string.msg_image_movie_require), Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (StringUtil.isEmpty(strImageBanner)) {
+            Toast.makeText(this, getString(R.string.msg_image_banner_movie_require), Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (StringUtil.isEmpty(strVideo)) {
+            Toast.makeText(this, getString(R.string.msg_video_movie_require), Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Update phim
+        if (isUpdate) {
+            showProgressDialog(true);
+            Map<String, Object> map = new HashMap<>();
+            map.put("name", strName);
+            map.put("description", strDescription);
+            map.put("price", Integer.parseInt(strPrice));
+            if (!strDate.equals(mMovie.getDate())) {
+                map.put("date", strDate);
+                map.put("rooms", GlobalFunction.getListRooms());
+            }
+            map.put("image", strImage);
+            map.put("imageBanner", strImageBanner);
+            map.put("url", strVideo);
+            map.put("categoryId", mCategorySelected.getId());
+            map.put("categoryName", mCategorySelected.getName());
+
+            MyApplication.get(this).getMovieDatabaseReference()
+                    .child(String.valueOf(mMovie.getId())).updateChildren(map, (error, ref) -> {
+                        showProgressDialog(false);
+                        Toast.makeText(AddMovieActivity.this, getString(R.string.msg_edit_movie_successfully), Toast.LENGTH_SHORT).show();
+                        GlobalFunction.hideSoftKeyboard(this);
+                    });
+            return;
+        }
+
+        // Add phim
+        showProgressDialog(true);
+        long movieId = System.currentTimeMillis();
+        Movie movie = new Movie(movieId, strName, strDescription, Integer.parseInt(strPrice),
+                strDate, strImage, strImageBanner, strVideo, GlobalFunction.getListRooms(),
+                mCategorySelected.getId(), mCategorySelected.getName(), 0);
+        MyApplication.get(this).getMovieDatabaseReference().child(String.valueOf(movieId)).setValue(movie, (error, ref) -> {
+            showProgressDialog(false);
+            mActivityAddMovieBinding.spnCategory.setSelection(0);
+            mActivityAddMovieBinding.edtName.setText("");
+            mActivityAddMovieBinding.edtDescription.setText("");
+            mActivityAddMovieBinding.edtPrice.setText("");
+            mActivityAddMovieBinding.tvDate.setText("");
+            mActivityAddMovieBinding.edtImage.setText("");
+            mActivityAddMovieBinding.edtImageBanner.setText("");
+            mActivityAddMovieBinding.edtVideo.setText("");
+            GlobalFunction.hideSoftKeyboard(this);
+            Toast.makeText(this, getString(R.string.msg_add_movie_successfully), Toast.LENGTH_SHORT).show();
+        });
+    }
 }
