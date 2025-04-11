0e2b9e4fbd064534af87f2009191260cf598fcf3
ngthuhuong <ngthuhuong0123@gmail.com>
2025-04-12 01:24:40 +0700

    improve : Category Act

--- app/src/main/java/com/example/cinemamanager/activity/CategoryActivity.java
+++ app/src/main/java/com/example/cinemamanager/activity/CategoryActivity.java
@@ -1,6 +1,7 @@
 package com.example.cinema.activity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,8 +24,9 @@ import java.util.List;
 public class CategoryActivity extends AppCompatActivity {
 
     private ActivityCategoryBinding mActivityCategoryBinding;
-    private List<Movie> mListMovies;
+    private List<Movie> mListMovies = new ArrayList<>();
     private Category mCategory;
+    private MovieAdapter movieAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,51 +34,58 @@ public class CategoryActivity extends AppCompatActivity {
         mActivityCategoryBinding = ActivityCategoryBinding.inflate(getLayoutInflater());
         setContentView(mActivityCategoryBinding.getRoot());
 
-        getDataIntent();
+        if (!getDataIntent()) {
+            finish(); // Close the activity if no category is provided
+            return;
+        }
+
         initListener();
+        setupRecyclerView();
         getListMovies();
     }
 
-    private void getDataIntent() {
+    private boolean getDataIntent() {
         Bundle bundleReceived = getIntent().getExtras();
         if (bundleReceived != null) {
             mCategory = (Category) bundleReceived.get(ConstantKey.KEY_INTENT_CATEGORY_OBJECT);
-            mActivityCategoryBinding.tvTitle.setText(mCategory.getName());
+            if (mCategory != null) {
+                mActivityCategoryBinding.tvTitle.setText(mCategory.getName());
+                return true;
+            }
         }
+        Log.e("CategoryActivity", "Category data is missing or null");
+        return false;
     }
 
     private void initListener() {
         mActivityCategoryBinding.imgBack.setOnClickListener(v -> onBackPressed());
     }
 
+    private void setupRecyclerView() {
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 3);
+        mActivityCategoryBinding.rcvData.setLayoutManager(gridLayoutManager);
+        movieAdapter = new MovieAdapter(mListMovies, movie -> GlobalFunction.goToMovieDetail(this, movie));
+        mActivityCategoryBinding.rcvData.setAdapter(movieAdapter);
+    }
+
     private void getListMovies() {
         MyApplication.get(this).getMovieDatabaseReference().addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if (mListMovies != null) {
-                    mListMovies.clear();
-                } else {
-                    mListMovies = new ArrayList<>();
-                }
+                mListMovies.clear();
                 for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
                     Movie movie = dataSnapshot.getValue(Movie.class);
-                    if (movie != null && mCategory.getId() == movie.getCategoryId()) {
+                    if (movie != null && mCategory != null && mCategory.getId() == movie.getCategoryId()) {
                         mListMovies.add(0, movie);
                     }
                 }
-                displayListMovies();
+                movieAdapter.notifyDataSetChanged();
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError error) {}
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e("CategoryActivity", "Failed to fetch movies: " + error.getMessage());
+            }
         });
-    }
-
-    private void displayListMovies() {
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 3);
-        mActivityCategoryBinding.rcvData.setLayoutManager(gridLayoutManager);
-        MovieAdapter movieAdapter = new MovieAdapter(mListMovies,
-                movie -> GlobalFunction.goToMovieDetail(this, movie));
-        mActivityCategoryBinding.rcvData.setAdapter(movieAdapter);
     }
 }
