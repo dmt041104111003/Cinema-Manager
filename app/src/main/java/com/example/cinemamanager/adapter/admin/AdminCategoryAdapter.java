e8ecfbb0ff84ac1098534bbd21af698d5331f8c2
dmt041104111003 <daomanhtung4102003@gmail.com @example.com>
2025-04-12 00:34:24 +0700

    adapter & model fix

--- app/src/main/java/com/example/cinemamanager/adapter/admin/AdminCategoryAdapter.java
+++ app/src/main/java/com/example/cinemamanager/adapter/admin/AdminCategoryAdapter.java
@@ -1,4 +1,4 @@
-package com.example.cinemamanager.adapter.admin;
+package com.example.cinema.adapter.admin;
 
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
@@ -6,9 +6,9 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.cinemamanager.databinding.ItemCategoryAdminBinding;
-import com.example.cinemamanager.model.Category;
-import com.example.cinemamanager.util.GlideUtils;
+import com.example.cinema.databinding.ItemCategoryAdminBinding;
+import com.example.cinema.model.Category;
+import com.example.cinema.util.GlideUtils;
 
 import java.util.List;
 
